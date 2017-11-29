ab287d30ac85d37dee897306049c01ce5ecfb8ce
FufitoP <carlos.murillo.badilla@est.una.ac.cr>
2017-11-28 21:26:01 -0600

    As I see
    I truthly doesn't know what kind of program I'm about to develop.

---
+++ JavaApplication40/src/javaapplication40/JavaApplication40.java
@@ -0,0 +1,11 @@
+package javaapplication40;
+
+public class JavaApplication40 {
+
+
+    public static void main(String[] args) {
+
+            System.out.println("Este cambio se notó?");
+    }
+    
+}
